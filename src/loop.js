254972fa213dcb5a30fbfcf22968685a9b9ac50a
Lyukx <lvkaixie@gmail.com>
2017-08-06 12:25:41 +0900

    add loop class

---
+++ src/loop.js
@@ -0,0 +1,7 @@
+// loopSet, stealthSet are aets of message ids
+// loopSet: messages displayed in the loop rectangles
+// stealthSet: messages not displayed
+export function Loop(loopSet, stealthSet){
+    this.loopSet = loopSet;
+    this.stealthSet = stealthSet;
+}
