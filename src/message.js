251eeaca83af0fab1af7d6c0ae799d31970bcb6c
Lyukx <lvkaixie@gmail.com>
2018-05-15 16:09:13 +0900

    remove useless attributes in message class

--- src/message.js
+++ src/message.js
@@ -1,21 +1,26 @@
 export function Message(rawMessage){
-    this.from = rawMessage.from;
+  this.from = rawMessage.from;
+  this.id = rawMessage.id;
+  this.count = rawMessage.count;
+  this.valid = false;
+  this.scale = 1;
+  this.position = 0;
+  this.fromOffset = 0;
+  this.toOffset = 0;
+  if(rawMessage.return != true){
     this.to = rawMessage.to;
     this.message = rawMessage.message;
-    this.id = rawMessage.id;
-    this.count = rawMessage.count;
-    this.valid = false;
-    this.scale = 1;
-    this.position = 0;
-    this.fromOffset = 0;
-    this.toOffset = 0;
+  }
+  else{
+    this.return = rawMessage.return;
+  }
 }
 
 Message.prototype.equals = function(another){
-    // position doesn't need to be same
-    return (this.from == another.from && this.to == another.to && this.message == another.message);
+  // position doesn't need to be same
+  return (this.from == another.from && this.to == another.to && this.message == another.message);
 }
 
 Message.prototype.isReturn = function(){
-    return (this.to == -1 && this.message.length == 0)
+  return this.return == true;
 }
