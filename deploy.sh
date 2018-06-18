d0e5cd673c129a5af4db284828fed236b2c9c46a
Lyukx <lvkaixie@gmail.com>
2018-06-18 16:06:04 +0900

    update deploy workflow

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
 
 npm run build
+echo "Copying into docs file..."
+cp -f ./dest/sd.js  ./docs/public/javascript/sd.js
+echo "Copying into template repository..."
 cp -f ./dest/sd.js  ../template-of-SDExplorer/public/javascripts/sd.js
